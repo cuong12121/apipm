4f7caab5fe604b69cf26abff8748913dd45171ee
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2024-05-04 11:05:46 +0700

    Update orderController.php

--- app/Http/Controllers/Backend/orderController.php
+++ app/Http/Controllers/Backend/orderController.php
@@ -10,7 +10,7 @@ class orderController extends Controller
 {
     public function getdata()
     {
-    	$data  = DB::table('fs_order_uploads_detail')->orderBy('id')->paginate(8);
+    	$data  = DB::table('fs_order_uploads_detail')->orderBy('id', 'desc')->paginate(8);
 
     
 
