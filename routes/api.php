b8fa5c207ec175488ebdcc9c4af606a8d916eb8a
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2024-05-05 18:51:02 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -15,8 +15,9 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+    Route::get('get-data-order-details', 'Backend\orderController@getdata');
 });
 Route::get('show-product-qualtity', 'sheetApiController@showQualtity');
 
-Route::get('get-data-order-details', 'Backend\orderController@getdata');
+
 
