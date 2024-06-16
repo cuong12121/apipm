7c94e7652d29c11b1aed8331eaaee7e3135c6066
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2024-06-16 17:02:08 +0700

    Update crawlController.php

--- app/Http/Controllers/crawlController.php
+++ app/Http/Controllers/crawlController.php
@@ -98,7 +98,7 @@ class crawlController extends Controller
                 $dem++;
                 $insert = ['file'=>$url, 'record_id'=>$value->id];
                 DB::table('check_error_pdf')->insert($insert);
-                echo $dem.'\n';
+                echo $dem.\n;
 
             }
             
