92728153de4fbf497ad47abad3be5971d01fe75b
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2024-07-09 18:08:43 +0700

    Update orderController.php

--- app/Http/Controllers/Backend/orderController.php
+++ app/Http/Controllers/Backend/orderController.php
@@ -46,7 +46,7 @@ class orderController extends Controller
 
     	$user_package_id = $request->name;
 
-    	if(!empty($date) && !empty($user_package_id)){
+    	if(!empty($date1) && !empty($user_package_id)&&!empty($user_package_id)){
     		
     		$data = DB::table('fs_order_uploads_detail')->where('is_package', 1)->where('user_package_id', $user_package_id)->whereBetween('date_package', [$startOfDay, $endOfDay])->get()->toArray();
 
