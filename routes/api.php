25f043b3320b5cf9a8dae455426a2a6925257414
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2024-09-08 20:47:26 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::get('search-data-user-id-package', 'Backend\orderController@SearchDataOfUser');
 
-Route::get('search-data-order-to-date','Backend\orderController@SearchDataOfOrder')
+Route::get('search-data-order-to-date','Backend\orderController@SearchDataOfOrder');
 
 
 Route::get('show-product-qualtity', 'sheetApiController@showQualtity');
