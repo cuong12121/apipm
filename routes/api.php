d3780b53834dd2706227f68797cda245c881b36b
cuong12121 <cuongdao12121@gmail.com>
2024-12-25 16:43:19 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -30,7 +30,7 @@ Route::get('search-data-order-to-date','Backend\orderController@searchDataToCode
 
 Route::get('show-product-qualtity', 'sheetApiController@showQualtity');
 
-Route::get('show-data-order-new', 'sheetApiController@show_order_details')
+Route::get('show-data-order-new', 'sheetApiController@show_order_details');
 
 Route::get('get-data-order-details', 'Backend\orderController@getdata');
 
