33d2b1c91dcf8c4f879b6ad1920a736380a9ba0b
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2025-01-20 20:53:45 +0700

    update

--- app/Http/Controllers/sheetApiController.php
+++ app/Http/Controllers/sheetApiController.php
@@ -935,7 +935,10 @@ class sheetApiController extends Controller
 
     public function show_order_details()
     {
-        $result = DB::table('fs_order_uploads_detail')->orderBy('id', 'desc')->limit(5000)->get();
+        // Ngày hiện tại và 30 ngày trước
+        $thirtyDaysAgo = Carbon::now()->subDays(30);
+
+        $result = DB::table('fs_order_uploads_detail')->orderBy('id', 'desc')->where('created_at', '>=', $thirtyDaysAgo)->get();
 
         $json = json_encode($result);
 
