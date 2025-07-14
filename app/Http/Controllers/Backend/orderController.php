4bff0593cbd9030b0dad97a5d9bcd2f5f138b2e9
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2025-07-14 08:03:32 +0700

    Update orderController.php

--- app/Http/Controllers/Backend/orderController.php
+++ app/Http/Controllers/Backend/orderController.php
@@ -213,7 +213,7 @@ class orderController extends Controller
     public function history_print()
     {
     	
-    	$data = DB::table('fs_order_uploads_history_prints')->where('created_time', $now)->where('warehouse_id', 6)->where('platform_id', 2)->orderBy('id', 'desc')->take(10)->get()->toArray();
+    	$data = DB::table('fs_order_uploads_history_prints')->where('warehouse_id', 6)->where('platform_id', 2)->orderBy('id', 'desc')->take(10)->get()->toArray();
     	$datas = json_encode($data);
     	return response($datas);
 
